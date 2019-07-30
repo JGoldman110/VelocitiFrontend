eae1636a597c7c2be71c964e03e25b67b8a04a29
Josh Goldman <jgman@umich.edu>
2019-07-30 11:35:57 -0400

    added strategy model

---
+++ src/app/models/strategy.model.ts
@@ -0,0 +1,11 @@
+export class Strategy {
+  id: number;
+  name: string;
+  type: string;
+  status: string;
+  ticker: string;
+  quantity: number;
+  limit: number;
+  stop: number;
+  position: string;
+}
