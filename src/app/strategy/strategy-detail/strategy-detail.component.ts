1b75ee7cb50582f2011c7f38c2159084fde875fd
Josh Goldman <jgman@umich.edu>
2019-07-30 10:32:24 -0400

    git tabs working on strat details comp

--- src/app/strategy/strategy-detail/strategy-detail.component.ts
+++ src/app/strategy/strategy-detail/strategy-detail.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-strategy-detail',
   templateUrl: './strategy-detail.component.html',
   styleUrls: ['./strategy-detail.component.css']
 })
-export class StrategyDetailComponent {
+export class StrategyDetailComponent implements OnInit {
+  showStatitics:boolen = true;
+  showTrades:boolean = false;
 
+  constructor() {
+
+  }
+
+  OnInit() {
+
+  }
+
+  toggelStatistics() {
+    this.showTrades = false;
+    this.showStatitics = true;
+  }
+
+  toggelTrades() {
+    this.showStatitics = false;
+    this.showTrades = true;
+  }
 }
