00d55a22f7b4e05dade24b2d12ed2bc8caf46f57
Josh Goldman <jgman@umich.edu>
2019-08-01 16:58:53 -0400

    percent pnl added

--- src/app/strategy/strategy-create/strategy-create.component.ts
+++ src/app/strategy/strategy-create/strategy-create.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-strategy-create',
   templateUrl: './strategy-create.component.html',
   styleUrls: ['./strategy-create.component.css']
 })
-export class StrategyCreateComponent {
+export class StrategyCreateComponent implements OnInit {
 
+  angForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.angForm = this.formBuilder.group({
+      name: [''],
+      type: [''],
+      ticker: [''],
+      quantity: [''],
+      slowAvgIntervale: [''],
+      fastAvgIntervale: [''],
+      limit: [''],
+      stop: ['']
+    });
+  }
+
+  onClickSubmit(email, password) {
+    alert('Your Email is : ' + email);
+  }
+
+  ngOnInit() {}
 }
